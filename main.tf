70d8c343a42951682aa28ad385d08e1783be8984
Nuwan Keshara <61798389+NuwanKeshara@users.noreply.github.com>
2026-04-25 18:10:34 +0300

    Refactor AWS resources setup
    Updated AWS resources to use 'nuwankeshara' naming convention and changed region to 'us-east-1'.

--- main.tf
+++ main.tf
@@ -1,56 +1,61 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "devopsshack_vpc" {
+# ---------------- VPC ----------------
+resource "aws_vpc" "nuwankeshara_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "devopsshack-vpc"
+    Name = "nuwankeshara-vpc"
   }
 }
 
-resource "aws_subnet" "devopsshack_subnet" {
-  count = 2
-  vpc_id                  = aws_vpc.devopsshack_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+# ---------------- Subnets ----------------
+resource "aws_subnet" "nuwankeshara_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.nuwankeshara_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.nuwankeshara_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "devopsshack-subnet-${count.index}"
+    Name = "nuwankeshara-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "devopsshack_igw" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# ---------------- Internet Gateway ----------------
+resource "aws_internet_gateway" "nuwankeshara_igw" {
+  vpc_id = aws_vpc.nuwankeshara_vpc.id
 
   tags = {
-    Name = "devopsshack-igw"
+    Name = "nuwankeshara-igw"
   }
 }
 
-resource "aws_route_table" "devopsshack_route_table" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# ---------------- Route Table ----------------
+resource "aws_route_table" "nuwankeshara_route_table" {
+  vpc_id = aws_vpc.nuwankeshara_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.devopsshack_igw.id
+    gateway_id = aws_internet_gateway.nuwankeshara_igw.id
   }
 
   tags = {
-    Name = "devopsshack-route-table"
+    Name = "nuwankeshara-route-table"
   }
 }
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "nuwankeshara_assoc" {
   count          = 2
-  subnet_id      = aws_subnet.devopsshack_subnet[count.index].id
-  route_table_id = aws_route_table.devopsshack_route_table.id
+  subnet_id      = aws_subnet.nuwankeshara_subnet[count.index].id
+  route_table_id = aws_route_table.nuwankeshara_route_table.id
 }
 
-resource "aws_security_group" "devopsshack_cluster_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+# ---------------- Security Groups ----------------
+resource "aws_security_group" "cluster_sg" {
+  vpc_id = aws_vpc.nuwankeshara_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +65,12 @@ resource "aws_security_group" "devopsshack_cluster_sg" {
   }
 
   tags = {
-    Name = "devopsshack-cluster-sg"
+    Name = "nuwankeshara-cluster-sg"
   }
 }
 
-resource "aws_security_group" "devopsshack_node_sg" {
-  vpc_id = aws_vpc.devopsshack_vpc.id
+resource "aws_security_group" "node_sg" {
+  vpc_id = aws_vpc.nuwankeshara_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,42 +87,13 @@ resource "aws_security_group" "devopsshack_node_sg" {
   }
 
   tags = {
-    Name = "devopsshack-node-sg"
+    Name = "nuwankeshara-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "devopsshack" {
-  name     = "devopsshack-cluster"
-  role_arn = aws_iam_role.devopsshack_cluster_role.arn
-
-  vpc_config {
-    subnet_ids         = aws_subnet.devopsshack_subnet[*].id
-    security_group_ids = [aws_security_group.devopsshack_cluster_sg.id]
-  }
-}
-
-resource "aws_eks_node_group" "devopsshack" {
-  cluster_name    = aws_eks_cluster.devopsshack.name
-  node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
-  subnet_ids      = aws_subnet.devopsshack_subnet[*].id
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
-  }
-
-  instance_types = ["t2.medium"]
-
-  remote_access {
-    ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.devopsshack_node_sg.id]
-  }
-}
-
-resource "aws_iam_role" "devopsshack_cluster_role" {
-  name = "devopsshack-cluster-role"
+# ---------------- IAM Cluster Role ----------------
+resource "aws_iam_role" "cluster_role" {
+  name = "nuwankeshara-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +111,14 @@ resource "aws_iam_role" "devopsshack_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_cluster_role_policy" {
-  role       = aws_iam_role.devopsshack_cluster_role.name
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "devopsshack_node_group_role" {
-  name = "devopsshack-node-group-role"
+# ---------------- IAM Node Role ----------------
+resource "aws_iam_role" "node_role" {
+  name = "nuwankeshara-node-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +136,49 @@ resource "aws_iam_role" "devopsshack_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_role_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "node_policy_1" {
+  role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_cni_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "node_policy_2" {
+  role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "devopsshack_node_group_registry_policy" {
-  role       = aws_iam_role.devopsshack_node_group_role.name
+resource "aws_iam_role_policy_attachment" "node_policy_3" {
+  role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# ---------------- EKS Cluster ----------------
+resource "aws_eks_cluster" "nuwankeshara_cluster" {
+  name     = "nuwankeshara-cluster"
+  role_arn = aws_iam_role.cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = aws_subnet.nuwankeshara_subnet[*].id
+    security_group_ids = [aws_security_group.cluster_sg.id]
+  }
+}
+
+# ---------------- Node Group ----------------
+resource "aws_eks_node_group" "nuwankeshara_node_group" {
+  cluster_name    = aws_eks_cluster.nuwankeshara_cluster.name
+  node_group_name = "nuwankeshara-node-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = aws_subnet.nuwankeshara_subnet[*].id
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  instance_types = ["t2.medium"]
+
+  remote_access {
+    ec2_ssh_key               = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.node_sg.id]
+  }
 }
