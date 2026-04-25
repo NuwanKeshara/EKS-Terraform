8ab7eb2eeee302eee7ea9560898a78d13964bc33
Nuwan Keshara <61798389+NuwanKeshara@users.noreply.github.com>
2026-04-25 18:11:03 +0300

    Update EKS cluster and node group outputs

--- output.tf
+++ output.tf
@@ -1,16 +1,23 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.nuwankeshara_cluster.id
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.nuwankeshara_cluster.name
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.nuwankeshara_node_group.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.nuwankeshara_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.nuwankeshara_subnet[*].id
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.nuwankeshara_cluster.endpoint
+}
